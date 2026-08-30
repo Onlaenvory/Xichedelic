2c920f10ba319411ab357ff8ae57353f9c0b9cd2
Onlaenvory <Onlaenvory@gmail.com>
2026-08-31 04:22:20 +0800

    #1.1 Add getaddrinfo error handling & Compleation of socket initialization

--- README.md
+++
@@ -1,3 +0,0 @@
-Overview
-
-addrinfo -> {ai_flags, ai_family, ai_socktype, ai_protocol, ai_addrlen}
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,44 @@
-#include <sys/socket.h>
-#include <netdb.h>
 #include "spdlog/spdlog.h"
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
 
-const char *url = "wss://stream.binance.com:9443/ws/bnbusd@aggTrade";
-const char *host = "stream.binance.com";
-const char *port = "9443";
+// const char *url = "wss://stream.binance.com:9443/ws/bnbusd@aggTrade";
+constexpr const char* host = "stream.binance.com";
+constexpr const char* port = "9443";
 
 int main() {
-  addrinfo SY;
-  addrinfo *receiver = nullptr;
-
-  SY.ai_flags = AI_PASSIVE;
-  SY.ai_socktype = SOCK_STREAM;
+  addrinfo SY{};
   SY.ai_family = AF_INET;
+  SY.ai_socktype = SOCK_STREAM;
+  SY.ai_protocol = IPPROTO_TCP; // Optional : SOCK_STREAM does select TCP automatically
 
-  int status = getaddrinfo(host, port, &SY, &receiver);
+  addrinfo* node = nullptr;
+  int status = getaddrinfo(host, port, &SY, &node);
 
-  spdlog::info("!");
-  spdlog::warn("!");
-  spdlog::error("!");
+  if (status != 0) {
+    spdlog::error("Invalid getaddrinfo : {}", gai_strerror(status));
+  };
+
+  spdlog::info("Valid getaddrinfo");
+
+  for(addrinfo* current_addr = node; current_addr != nullptr; current_addr->ai_next) {
+    int sock_fdesc = socket(current_addr->ai_family, current_addr->ai_socktype, current_addr->ai_protocol);
+    spdlog::info("Socket Created");
+
+    if (sock_fdesc == -1) {
+      spdlog::info("Incompatible address, Hoping address");
+      continue;
+    }
+
+    spdlog::info("Compatible address found, Connecting session");
+    if (connect(sock_fdesc, current_addr->ai_addr, current_addr->ai_addrlen)) {
+      spdlog::info("Connected Succefully");
+      break;
+    }
+
+    close(sock_fdesc);
+  }
 
   return 0;
 }
